0a50a354d499bcd920eb701c0cf896fb957381b8
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-03-03 19:07:38 +0545

    add sqlite for expo

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import "./styles.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello universe!</Text>
+    <View className="flex-1 items-center justify-center bg-yellow font-bold uppercase">
+      <Text>hello for the fuck sake!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
